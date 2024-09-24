b918e3ebbfff7b1f331c272b11d968009ea3bada
Sultan Alsawaf <sultan@kerneltoast.com>
2024-09-23 23:26:03 -0700

    sched/fair: Iterate in ascending CPU order when doing idle load balance
    On asymmetric arm64 systems, CPUs are enumerated by ascending order of
    capacity. Since the idle load balancer doesn't iterate through possible CPU
    candidates starting from the beginning of the idle CPU mask, higher
    capacity CPUs may receive tasks before qualified lower capacity CPUs. This
    results in a small hit to energy efficiency, since higher capacity CPUs are
    typically far less efficient than lower capacity CPUs.
    
    Iterate through the small CPUs first as possible load-balance destination
    CPUs, so that tasks are more likely to be migrated to the lowest capacity
    CPU that fits them, thus saving energy.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Tashar02: Don't remove "this_cpu" since idle_cpu() is a user of it]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11157,9 +11157,7 @@ static bool _nohz_idle_balance(struct rq *this_rq, unsigned int flags,
 	smp_mb();
 
 	for_each_cpu(balance_cpu, nohz.idle_cpus_mask) {
-		if (balance_cpu == this_cpu || !idle_cpu(balance_cpu))
-			continue;
-		if (cpu_isolated(balance_cpu))
+		if (!idle_cpu(balance_cpu))
 			continue;
 		/*
 		 * If this CPU gets work to do, stop the load balancing
