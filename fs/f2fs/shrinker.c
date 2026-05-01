ca3cc4b611a625bd127fee1707cc9aa344ec92cd
raphakkj4 <raphaelfox583@gmail.com>
2026-05-01 13:58:07 -0300

    Revert "f2fs: optimize extent cache and relax shrinker"
    This reverts commit 588ebfa09c6fec35ab556f98a5252ead0df743c7.

--- fs/f2fs/shrinker.c
+++ fs/f2fs/shrinker.c
@@ -102,7 +102,7 @@ unsigned long f2fs_shrink_scan(struct shrinker *shrink,
 		sbi->shrinker_run_no = run_no;
 
 		/* shrink extent cache entries */
-		freed += f2fs_shrink_extent_tree(sbi, nr >> 2);
+		freed += f2fs_shrink_extent_tree(sbi, nr >> 1);
 
 		/* shrink clean nat cache entries */
 		if (freed < nr)
